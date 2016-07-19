2922ac4c9fbc8a778912a126f6b65ec34b37fae5
noppoman <yuki@miketokyo.com>
2016-07-20 02:36:47 +0900

    3.0-PREVIEW-2

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
 	name: "Suv",
 	dependencies: [
-      .Package(url: "https://github.com/noppoMan/swifty-libuv.git", majorVersion: 0, minor: 1),
+      .Package(url: "https://github.com/noppoMan/swifty-libuv.git", majorVersion: 0, minor: 2),
       .Package(url: "https://github.com/slimane-swift/Time.git", majorVersion: 0, minor: 2),
-      .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 8),
+      .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 9),
   ],
   targets: [
       Target(
