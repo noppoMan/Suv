493229029218a0e6aa2d7a24fa0d3de52c38a822
noppoman <yuki@miketokyo.com>
2016-09-11 15:06:26 +0900

    Updating to swift-09-10

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ let package = Package(
         Target(name: "ClusterTest", dependencies: ["Suv"])
     ],
 	dependencies: [
-      .Package(url: "https://github.com/noppoMan/swifty-libuv.git", majorVersion: 0, minor: 3),
-      .Package(url: "https://github.com/slimane-swift/Time.git", majorVersion: 0, minor: 2),
+      .Package(url: "https://github.com/noppoMan/swifty-libuv.git", majorVersion: 0, minor: 4),
+      .Package(url: "https://github.com/slimane-swift/Time.git", majorVersion: 0, minor: 3),
       .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 12)
   ]
 )
